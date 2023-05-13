cad9959a6fbe31c951126f9bbc59647a28bf1318
skkzsh <skkzsh12@gmail.com>
2023-05-13 21:29:37 +0900

    security_and_analysis で差分が出るようになった

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -9,6 +9,20 @@ resource "github_repository" "this" {
   has_issues   = try(var.repository_options.has_issues, null)
   homepage_url = try(var.repository_options.homepage_url, null)
 
+  security_and_analysis {
+    advanced_security {
+      status = "enabled"
+    }
+
+    secret_scanning {
+      status = "enabled"
+    }
+
+    secret_scanning_push_protection {
+      status = "enabled"
+   }
+}
+
   lifecycle {
     prevent_destroy = true
   }
