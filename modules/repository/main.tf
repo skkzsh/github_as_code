0cd4bac6ef1839b3b0e86c5203ed04fae0528847
skkzsh <skkzsh12@gmail.com>
2024-08-30 20:35:18 +0900

    リポジトリ作成時にブランチ作成 (と空コミット)

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -31,3 +31,21 @@ resource "github_repository" "this" {
     prevent_destroy = true
   }
 }
+
+# resource "terraform_data" "initial_commit" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       git clone https://github.kddi.com/${data.github_user.current.id}/${github_repository.this.name}.git
+#       cd ${github_repository.this.name}
+#       git commit --allow-empty -m "Create repository"
+#       git push -u origin main
+#       # rm -rf ${github_repository.this.name}
+#     EOT
+#   }
+#
+#   triggers_replace = {
+#     repository_name = github_repository.this.name
+#   }
+#
+#   depends_on = [github_repository.this]
+# }
