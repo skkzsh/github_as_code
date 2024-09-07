614004e4475b408067bbf94dcb2b0a8ed9c386d2
skkzsh <skkzsh12@gmail.com>
2024-09-08 01:10:54 +0900

    Update PR config

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -1,18 +1,21 @@
 resource "github_repository" "this" {
-  name        = var.repository_name
-  description = try(var.repository_options.description, null)
-  topics      = try(var.repository_options.topics, null)
-
-  vulnerability_alerts = true
-  has_projects         = false # TODO
-  has_downloads        = true  # TODO
-
-  has_wiki     = try(var.repository_options.has_wiki, true)
-  has_issues   = try(var.repository_options.has_issues, null)
+  name         = var.repository_name
+  description  = try(var.repository_options.description, null)
+  topics       = try(var.repository_options.topics, null)
   homepage_url = try(var.repository_options.homepage_url, null)
+
+  has_wiki      = try(var.repository_options.has_wiki, true)
+  has_issues    = try(var.repository_options.has_issues, null)
+  has_projects  = false # TODO
+  has_downloads = true  # TODO
 
   allow_rebase_merge = false
   allow_squash_merge = false
+
+  allow_update_branch    = true
+  delete_branch_on_merge = true
+
+  vulnerability_alerts = true
 
   security_and_analysis {
     # advanced_security {
